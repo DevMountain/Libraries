d3d46641f90b2b5e80286367975e73590485b541
Andrew Madsen <andrew@openreelsoftware.com>
2016-09-21 11:52:37 -0600

    Initial commit.

---
+++ Sources/DMNAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Libraries
+//
+//  Created by Andrew Madsen on 9/21/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Sources/DMNAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Libraries
+//
+//  Created by Andrew Madsen on 9/21/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+#import "DMNAppDelegate.h"
+
+@interface DMNAppDelegate ()
+
+@end
+
+@implementation DMNAppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+	return YES;
+}
+
+@end
